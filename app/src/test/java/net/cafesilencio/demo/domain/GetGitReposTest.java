71d5df4d557b7f3aadd3d78afaf2997745f4725c
seth bourget <seth@cafesilencio.net>
2018-04-14 16:59:42 -0700

    wip

---
+++ app/src/test/java/net/cafesilencio/demo/domain/GetGitReposTest.java
@@ -0,0 +1,17 @@
+package net.cafesilencio.demo.domain;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Seth Bourget on 4/14/18.
+ */
+public class GetGitReposTest {
+
+
+    @Test
+    public void call() {
+        assertTrue(false);
+    }
+}
